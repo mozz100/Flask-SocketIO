5ee96228558e13014dc73ddfd45742f05e8784c9
Richard Morrison <richard@owlstone.co.uk>
2014-04-28 15:05:54 +0100

    Improve serverside client

--- example/serverside_client.py
+++ example/serverside_client.py
@@ -1,21 +1,29 @@
-import logging
-
-#logging.basicConfig(level=logging.DEBUG)
-
 from socketIO_client import SocketIO, BaseNamespace
+from gevent.socket import wait_read, timeout
+import sys
+import gevent
+from gevent import monkey
+
+monkey.patch_socket() # othersie socketIO.wait seems to block
 
 class TestNamespace(BaseNamespace):
-
     def on_my_response(self, *args):
-        print 'on_my_response', args
+        print args
 
 socketIO = SocketIO('localhost', 5000)
 test_namespace = socketIO.define(TestNamespace, '/test')
 
-count = 0
+def stdin_reader():
+    count = 0
+    while True:
+        count += 1
+        try:
+            wait_read(sys.stdin.fileno(), timeout=3)
+            line = sys.stdin.readline().strip()
+            test_namespace.emit('my broadcast event', {'data': line, 'count': count})
+        except timeout:
+            test_namespace.emit('my broadcast event', {'data': 'tick', 'count': count})
 
-while True:
-    count += 1
-    test_namespace.emit('my broadcast event', {'data': 'server event', 'count': count})
-    socketIO.wait(seconds=10)
+greenlets = [gevent.Greenlet.spawn(f) for f in (socketIO.wait, stdin_reader)]
 
+gevent.joinall(greenlets)
